a2ef2121c060f7fa3f9a30532b51087a0005e5e4
violetakokorevi <91872388+violetakokorevicha@users.noreply.github.com>
2022-09-07 13:09:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+
+a = 23
+b = 45
+if a > b:
+  print('A')
+else:
+  print('B')
+
+
+
+
+a = int(input("a:"))
+b = 45
+if a > b:
+  print('a is greater tha b')
+if a < b:
+  print('a is smaller than b')
+if a == b:
+  print(" a is eqaul to b")
+else:
+   print(" a is not eqaul to b")
+
+
+
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+
+
